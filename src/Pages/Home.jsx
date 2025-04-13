1a9a2c6cde02fbaa0acca491e8e14ecb721e00da
Sheetu1 <sheetusharma548@gmail.com>
2025-04-14 00:46:03 +0530

    DataFetch Complete

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="w-full min-h-screen px-10 py-10 bg-gray-100">
+    <div className="w-full min-h-screen px-8 py-8 bg-gray-100">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
         {products.map((item) => (
           <div
